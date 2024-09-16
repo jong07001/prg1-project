582c94b21f788d70c1a8464c6bd61338ac27d054
jong07001 <jong07001@utb.vaxjo.se>
2024-09-16 09:42:41 +0200

    Gissningsspel
    Gissningspel mellan med tal mellan 1-100
    Visar högre eller lägre samt antalet gissningar som krävdes

--- obj/Debug/net8.0/prg1-project.AssemblyInfo.cs
+++ obj/Debug/net8.0/prg1-project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prg1-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+829f95fa5ab714b0505cee4719907f377213f399")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c7468351756e2b59239ed01d404d8123cace665")]
 [assembly: System.Reflection.AssemblyProductAttribute("prg1-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prg1-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
