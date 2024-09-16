f1dd5a9c5981ef92038bc6f9111dba03f45e0117
jong07001 <jong07001@utb.vaxjo.se>
2024-09-16 10:09:12 +0200

    Gissningsspel
    Gissningsspel mellan 1-100
    Ger en ledtråd om högre eller lägre
    Säger rätt svar och antal gissningar som krävdes

---
+++ C# Inlämning guess game/Program.cs
@@ -0,0 +1,22 @@
+﻿{
+    Random rnd = new Random();
+    int nr = rnd.Next(1,101);
+    int gissning = 0;
+    int gissningar = 0;
+    Console.WriteLine("Gissa vilket tal");
+    while(gissning != nr){
+            gissning = Convert.ToInt32(Console.ReadLine());
+            gissningar++;
+        if(gissning < nr){
+            Console.WriteLine("talet är för lågt");
+        }
+        else if(gissning>nr){
+            Console.WriteLine("talet är för högt");
+        }
+    }
+    
+    Console.WriteLine("talet är korrekt, du använde " + gissningar + " gissningar");
+
+    
+
+}
